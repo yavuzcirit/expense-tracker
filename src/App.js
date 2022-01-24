5cfafbaf8d9dc0243d593d49313f40c148440a92
Yavuz Cirit <yavuzcirit23@gmail.com>
2022-01-24 17:25:38 +0300

    UI and expenses founded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/expenses/Expenses"
 
-const App= () => {
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -26,25 +27,9 @@ const App= () => {
 
   return (
     <div className="App">
-      <h2>Expenses Tracker</h2>
-      <ExpenseItem 
-      title={expenses[0].title}
-      amount={expenses[0].amount}
-      date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      ></ExpenseItem> <ExpenseItem 
-      title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date}
-      ></ExpenseItem> <ExpenseItem 
-      title={expenses[3].title}
-      amount={expenses[3].amount}
-      date={expenses[3].date}
-      ></ExpenseItem>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
+
     </div>
   );
 }
